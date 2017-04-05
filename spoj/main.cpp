28d6b067074807a19d8d12c7b12616b9e2a35034
Nikhil Jindal <nik.jindal20@gmail.com>
2017-04-05 23:19:27 +0530

    Add files via upload

---
+++ spoj/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void ans(int a,long long int b)
+{
+    int res=1;
+    a=a%10;
+    while(b>0)
+    {
+        if(b%2==1)
+          res=(res*a)%10;
+        b/=2;
+        a=(a*a)%10;
+    }
+    cout<<res;
+}
+int main()
+{
+    int t,a;
+    long long int b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>a>>b;
+        ans(a,b);
+        cout<<"\n";
+    }
+    return 0;
+}
