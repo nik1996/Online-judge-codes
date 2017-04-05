bee40b0b330cf467cb69f33a946424e910d9ca43
Nikhil Jindal <nik.jindal20@gmail.com>
2017-04-05 23:10:28 +0530

    Add files via upload

---
+++ spoj/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long long int t,n,x,sum,m[1010],w[1010],i;
+    cin>>t;
+    while(t--)
+    {
+        sum=0;
+        cin>>n;
+        for(i=0;i<n;i++)
+          cin>>m[i];
+        for(i=0;i<n;i++)
+          cin>>w[i];
+        sort(m,m+n);
+        sort(w,w+n);
+        for(i=0;i<n;i++)
+        {
+            x=m[i]*w[i];
+            sum=sum+x;
+        }
+        cout<<sum<<"\n";
+    }
+    return 0;
+}
