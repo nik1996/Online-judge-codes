0545982f0991c18826ed19aa17a4ff29dcdc47b2
Nikhil Jindal <nik.jindal20@gmail.com>
2017-04-07 20:02:35 +0530

    Add files via upload

---
+++ spoj/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int v[5010],l,i;
+    while(1)
+    {
+        cin>>s;
+        if(s=="0")
+        break;
+        l=s.size();
+        //cout<<l<<"L\n";
+        memset(v,0,sizeof(int)*(l+5));
+        v[l]=v[l+1]=v[l-1]=1;
+        for(i=l-2;i>=0;i--)
+        {
+            if(s[i]=='0')
+            {
+                //i--;
+                continue;
+            }
+            else if(s[i+1]=='0')
+            {
+                 v[i]=v[i+2];
+                 //cout<<v[i]<<" "<<i<<"I\n";
+            }
+            else
+            {
+                if(((s[i]-48)*10+(s[i+1]-48))<=26&&s[i+2]!='0')
+                 v[i]=v[i+1]+v[i+2];
+                else
+                 v[i]=v[i+1];
+                //cout<<v[i]<<"I\n";
+            }
+        }
+        printf("%d\n",v[0]);
+    }
+    return 0;
+}
