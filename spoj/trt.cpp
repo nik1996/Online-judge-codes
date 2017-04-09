397208d16b66c3b1da6592931cc3228b12837cd5
Nikhil Jindal <nik.jindal20@gmail.com>
2017-04-09 23:26:43 +0530

    Update and rename main.cpp to trt.cpp

--- spoj/trt.cpp
+++ spoj/trt.cpp
@@ -1,3 +1,7 @@
+//Problem Link : http://www.spoj.com/problems/TRT/
+
+************************************************************************************************************************************
+
 #include <iostream>
 #include <cstdio>
 #include <algorithm>
