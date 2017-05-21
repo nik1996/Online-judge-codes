f967adb15901cba1e0d24e73eced5c05a93e3ebc
Nikhil Jindal <nik.jindal20@gmail.com>
2017-05-21 19:39:14 +0530

    Add files via upload

---
+++ spoj/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+    long long int t,n,k,r,i;
+    scanf("%lld",&t);
+    while(t--)
+    {
+        r=1;
+        scanf("%lld%lld",&n,&k);
+        if(k>(n-k+1))
+         k=n-k+1;
+        for(i=1;i<k;i++)
+        {
+            r*=(n-i);
+            r/=i;
+        }
+        printf("%lld\n",r);
+    }
+    return 0;
+}
