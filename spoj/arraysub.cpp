29cc9507fee88d3a931d449bba7a9b9d5b9b3121
Nikhil Jindal <nik.jindal20@gmail.com>
2017-05-22 20:46:15 +0530

    Update and rename main.cpp to arraysub.cpp

--- spoj/arraysub.cpp
+++ spoj/arraysub.cpp
@@ -1,3 +1,7 @@
+//Problem URL : http://www.spoj.com/problems/ARRAYSUB/
+
+*************************************************************************************************************************************
+
 #include <iostream>
 #include <cstdio>
 #include <deque>
