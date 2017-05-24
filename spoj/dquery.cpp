4c300bdd82e1f61f7cdb57836e59306b49d3fc72
Nikhil Jindal <nik.jindal20@gmail.com>
2017-05-24 22:36:27 +0530

    Update and rename main.cpp to dquery.cpp

--- spoj/dquery.cpp
+++ spoj/dquery.cpp
@@ -1,3 +1,7 @@
+//Problem URL : http://www.spoj.com/problems/DQUERY/
+
+***********************************************************************************************************************************
+
 #include <iostream>
 #include <cstdio>
 #include <cmath>
